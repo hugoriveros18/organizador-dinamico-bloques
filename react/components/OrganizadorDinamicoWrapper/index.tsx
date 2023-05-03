bad24d94534870d990b7ee6ec3003672fbef5176
hugo riveros <hugo.riveros@itglobers.com>
2023-05-03 12:35:23 -0500

    update

--- react/components/OrganizadorDinamicoWrapper/index.tsx
+++ react/components/OrganizadorDinamicoWrapper/index.tsx
@@ -1,31 +1,22 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import OrganizadorDinamicoWrapperSchema from '../../schema/OrganizadorDinamicoWrapperSchema';
+import Loading from '../Loading';
+import useBlocksOrganizer from '../../hooks/useBlocksOrganizer';
 
 const OrganizadorDinamicoWrapper = ({disposicionBloques,children}:OrganizadorDinamicoWrapperProps) => {
 
-  //STATES
-  const [currentChildren, setCurrentChildren] = useState<any>([]);
-
-  //EFFECTS
-  useEffect(() => {
-    const childrensActivos = disposicionBloques.map((bloque) =>  {
-      if(bloque.configuracionVisibilidad.posiblesConfiguraciones === 'Activacion Manual' && bloque.configuracionVisibilidad.estaActivo) {
-        return children[bloque.indexId];
-      }
-      if(bloque.configuracionVisibilidad.fechaInicio && bloque.configuracionVisibilidad.fechaFinal) {
-        const now = new Date();
-        const fechaIncioInput = new Date(bloque.configuracionVisibilidad.fechaInicio);
-        const fechaFinalInput = new Date(bloque.configuracionVisibilidad.fechaFinal);
-        if(fechaIncioInput.getTime() < now.getTime() && fechaFinalInput.getTime() > now.getTime()) {
-          return children[bloque.indexId]
-        }
-      }
-      return null;
-    })
-    setCurrentChildren(childrensActivos);
-  },[])
+  //BLOCKS ORGANIZATION
+  const { currentChildren, loading } = useBlocksOrganizer({
+    disposicionBloques: disposicionBloques,
+    children: children
+  })
 
   //JSX
+  if(loading) {
+    return (
+      <Loading />
+    )
+  }
   return (
     <>{currentChildren}</>
   )
---
+++ react/hooks/useBlocksOrganizer.ts
@@ -0,0 +1,33 @@
+import { useState, useEffect } from "react";
+
+export default function useBlocksOrganizer({disposicionBloques, children}:OrganizadorDinamicoWrapperProps) {
+
+  //STATES
+  const [currentChildren, setCurrentChildren] = useState<any>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+
+  //EFFECTS
+  useEffect(() => {
+    const childrensActivos = disposicionBloques.map((bloque) =>  {
+      if(bloque.configuracionVisibilidad.posiblesConfiguraciones === 'Activacion Manual' && bloque.configuracionVisibilidad.estaActivo) {
+        return children[bloque.indexId];
+      }
+      if(bloque.configuracionVisibilidad.fechaInicio && bloque.configuracionVisibilidad.fechaFinal) {
+        const now = new Date();
+        const fechaIncioInput = new Date(bloque.configuracionVisibilidad.fechaInicio);
+        const fechaFinalInput = new Date(bloque.configuracionVisibilidad.fechaFinal);
+        if(fechaIncioInput.getTime() < now.getTime() && fechaFinalInput.getTime() > now.getTime()) {
+          return children[bloque.indexId]
+        }
+      }
+      return null;
+    })
+    setCurrentChildren(childrensActivos);
+    setLoading(false);
+  },[])
+
+  return {
+    loading: loading,
+    currentChildren: currentChildren
+  }
+}
